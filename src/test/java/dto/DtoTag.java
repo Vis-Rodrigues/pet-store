cfcf89bcd3230b7ee1883c9305970cb1edc16b3f
Administrator <servicoinmetrics734472@userede.com.br>
2021-02-14 14:43:40 -0300

    Adicionado alguns testes de order

---
+++ src/test/java/dto/DtoTag.java
@@ -0,0 +1,29 @@
+package dto;
+
+public class DtoTag {
+
+	private int id;
+	private String name;
+	
+	public DtoTag(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	
+	public DtoTag() {}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
